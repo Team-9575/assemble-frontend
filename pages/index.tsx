a063c00316ee8ac20be3234390cbd50bb21cd58c
Hyeon1445 <lily1445@gmail.com>
2022-09-23 20:50:24 +0900

    Initial Commit

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+import styled from '@emotion/styled'
+
+const Home: NextPage = () => {
+  return <Container>main page</Container>
+}
+
+const Container = styled.div`
+  background-color: yellow;
+`
+export default Home
