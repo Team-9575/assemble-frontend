52251d4ddd3ceecab9fb7c1f68c8ab25497f5129
Hyeon1445 <lily1445@gmail.com>
2022-11-13 11:46:31 +0900

    chore: change directory path

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import '../styles/globals.css'
+import '@styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from '@hooks/context/useTheme'
 import { ThemeProvider as MUIThemeProvider, createTheme } from '@mui/material'
