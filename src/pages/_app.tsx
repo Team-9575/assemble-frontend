69eec96f754dc993b705b36081dcd64ced0a0954
Hyeon1445 <76871403+Hyeon1445@users.noreply.github.com>
2023-01-01 21:19:03 +0900

    fix: fix auto login & logout issue (#42)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,34 +2,46 @@ import '@styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from '@hooks/context/useTheme'
 import { ThemeProvider as MUIThemeProvider, createTheme } from '@mui/material'
-import { PublicClientApplication } from '@azure/msal-browser'
+import { InteractionStatus, PublicClientApplication } from '@azure/msal-browser'
 import { MsalProvider, useIsAuthenticated, useMsal } from '@azure/msal-react'
 import { msalConfig } from '@config/auth'
 import { AuthProvider } from '@hooks/context/useAuth'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import Head from 'next/head'
-import { AxiosError } from 'axios'
+import axios, { AxiosError } from 'axios'
 import apiClient, { HttpStatusCode } from 'src/api'
 import { AuthResponseType } from '@hooks/query/auth/useAuthMutation'
 import Cookies from 'js-cookie'
 
 export const msalInstance = new PublicClientApplication(msalConfig)
 
+export const logout = async () => {
+  Cookies.remove('csrftoken')
+  console.log('logout!!')
+  await apiClient.post('/logout')
+}
+
 const Components = ({ Component, pageProps }: any) => {
   const isMSAuthenticated = useIsAuthenticated()
-  const { instance, accounts, inProgress } = useMsal()
+  const { accounts, inProgress } = useMsal()
+
   const muiTheme = createTheme({
     typography: {
       fontFamily: 'Pretendard',
     },
   })
+
   const refreshToken = async (error: any) => {
-    let token = null
-    if (!isMSAuthenticated) {
-      Cookies.remove('csrftoken')
-      // logout
+    if (!isMSAuthenticated && inProgress === InteractionStatus.None) {
+      logout()
+      console.log(
+        'logout - refreshToken - !isMSAuthenticated',
+        isMSAuthenticated,
+        inProgress
+      )
       return
     }
+    let token = null
     if (accounts.length) {
       const { homeAccountId, environment, idTokenClaims } = accounts[0]
       const sessionKey = `${homeAccountId}-${environment}-refreshtoken-${idTokenClaims?.aud}----`
@@ -37,7 +49,12 @@ const Components = ({ Component, pageProps }: any) => {
       token = sessionValue && JSON.parse(sessionValue).secret
     }
     if (!token) {
-      // logout
+      // logout()
+      // console.log(
+      //   'logout - refreshToken - no ms token',
+      //   isMSAuthenticated,
+      //   inProgress
+      // )
       return
     }
     try {
@@ -46,36 +63,36 @@ const Components = ({ Component, pageProps }: any) => {
       })
       return data
     } catch (error) {
-      // logout
+      logout()
+      console.log('refreshToken - axios error')
       return Promise.reject(error as AxiosError)
     }
   }
+
+  const handleRetry = (failureCount: number, error: any) => {
+    if (
+      failureCount < 3 &&
+      error?.response?.status === HttpStatusCode.Unauthorized
+    ) {
+      refreshToken(error)
+      return true
+    }
+    return false
+  }
+
   const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
         refetchOnWindowFocus: false,
-        retry: (failureCount, error: any) => {
-          if (
-            failureCount < 3 &&
-            error?.response?.status === HttpStatusCode.Unauthorized
-          ) {
-            refreshToken(error)
-            return true
-          }
-          return false
-        },
+        refetchOnMount: false,
+        retry: handleRetry,
       },
       mutations: {
-        retry: (failureCount, error: any) => {
-          if (failureCount < 3 && error?.response?.status === 403) {
-            refreshToken(error)
-            return true
-          }
-          return false
-        },
+        retry: handleRetry,
       },
     },
   })
+
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider>
