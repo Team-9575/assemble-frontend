7b5320c98153af0fb28febc8402dd068211a117a
Hyeon1445 <lily1445@gmail.com>
2023-01-09 01:14:03 +0900

    feat: implement full receipt modal

--- src/pages/review/index.tsx
+++ src/pages/review/index.tsx
@@ -1,11 +1,13 @@
 import BaseLayout from '@components/common/base-layout'
 import Button from '@components/common/button'
 import VStack from '@components/common/stack/VStack'
+import FullReceipt from '@components/party-detail/FullReceipt'
 import IndividualReceipt from '@components/party-detail/IndividualReceipt'
 import { useState } from 'react'
 
 const MyReviewPage = () => {
   const [isIndividualReceiptOpen, setIsIndividualReceiptOpen] = useState(false)
+  const [isFullReceiptOpen, setIsFullReceiptOpen] = useState(false)
   return (
     <BaseLayout>
       <VStack gap="1rem" padding="1rem">
@@ -26,7 +28,15 @@ const MyReviewPage = () => {
         <Button
           variant="outlined"
           text="파티장 영수증(전체)"
-          onClick={() => {}}
+          onClick={() => {
+            setIsFullReceiptOpen(true)
+          }}
+        />
+        <FullReceipt
+          isOpen={isFullReceiptOpen}
+          onClose={() => {
+            setIsFullReceiptOpen(false)
+          }}
         />
         <Button variant="outlined" text="리뷰" onClick={() => {}} />
       </VStack>
