f9329b013788799c29dcff7f1c8f97e6aa9a82bf
Hyeon1445 <lily1445@gmail.com>
2023-01-08 16:29:48 +0900

    feat: apply category selector style

--- src/components/party/modal/CategorySelect.tsx
+++ src/components/party/modal/CategorySelect.tsx
@@ -1,8 +1,11 @@
 import Button from '@components/common/button'
+import InputFormik from '@components/common/formik/InputFormik'
 import styled from '@emotion/styled'
 import { theme } from '@styles/theme'
-import { Dispatch, SetStateAction } from 'react'
+import { Form, Formik, yupToFormErrors } from 'formik'
+import { Dispatch, SetStateAction, useState } from 'react'
 import { ModalContainer, ModalFooter, Step, Title } from '.'
+import * as Yup from 'yup'
 
 interface ICategorySelectProps {
   setCurrentStep: Dispatch<SetStateAction<Step>>
@@ -57,30 +60,64 @@ const categories = [
     name: '직접 입력',
   },
 ]
+const initialCategory = {
+  category: '', // TODO: fetch api
+  customCategory: '',
+}
+
+const validationSchema = Yup.object({
+  category: Yup.string(),
+  customCategory: Yup.string(),
+})
 
 const CategorySelect = ({ setCurrentStep }: ICategorySelectProps) => {
+  const [selectedCategory, setSelectedCategory] = useState('')
+
   return (
-    <>
-      <ModalContainer>
-        <Title>메뉴 카테고리를 선택해 주세요.</Title>
-        <CategoryContainer>
-          {categories.map((category, index) => (
-            <Category key={`category-${index}`}>
-              <CategoryIcon>{category.icon}</CategoryIcon>
-              <CategoryName>{category.name}</CategoryName>
-            </Category>
-          ))}
-        </CategoryContainer>
-      </ModalContainer>
-      <ModalFooter>
-        <Button
-          text="나중에 선택하기"
-          onClick={() => {
-            setCurrentStep(Step.Required)
-          }}
-        />
-      </ModalFooter>
-    </>
+    <Formik
+      initialValues={initialCategory}
+      validationSchema={validationSchema}
+      onSubmit={(values) => {}}
+    >
+      {({ errors, touched, values, isValid, setFieldValue }) => (
+        <Form>
+          <ModalContainer>
+            <Title>메뉴 카테고리를 선택해 주세요.</Title>
+            <CategoryContainer>
+              {categories.map((category, index) => (
+                <Category
+                  key={`category-${index}`}
+                  hasSelectedCategory={!!selectedCategory}
+                  onClick={() => {
+                    setSelectedCategory(
+                      selectedCategory === category.name ? '' : category.name
+                    )
+                  }}
+                  isSelected={selectedCategory === category.name}
+                >
+                  <CategoryIcon>{category.icon}</CategoryIcon>
+                  <CategoryName>{category.name}</CategoryName>
+                </Category>
+              ))}
+            </CategoryContainer>
+            {selectedCategory === '직접 입력' && (
+              <InputFormik
+                label="직접 카테고리를 입력해주세요."
+                name="customCategory"
+              />
+            )}
+          </ModalContainer>
+          <ModalFooter>
+            <Button
+              text="나중에 선택하기"
+              onClick={() => {
+                setCurrentStep(Step.Required)
+              }}
+            />
+          </ModalFooter>
+        </Form>
+      )}
+    </Formik>
   )
 }
 
@@ -88,16 +125,25 @@ const CategoryContainer = styled.div`
   display: grid;
   gap: 0.5rem;
   grid-template-columns: 1fr 1fr 1fr;
-  margin-top: 1.75rem;
+  margin: 1.75rem 0 0.5rem 0;
 `
-const Category = styled.div`
+const Category = styled.div<{
+  hasSelectedCategory: boolean
+  isSelected: boolean
+}>`
   height: 5.5rem;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  border: 1px solid #dbdbdb; // TODO: theme
+  border: ${({ isSelected }) =>
+    isSelected ? '1px solid #424242' : '1px solid #dbdbdb'};
   border-radius: 8px;
+  box-shadow: ${({ isSelected }) =>
+    isSelected && '0px 0px 8px rgba(0, 0, 0, 0.16)'};
+  cursor: pointer;
+  opacity: ${({ hasSelectedCategory, isSelected }) =>
+    hasSelectedCategory && !isSelected ? 0.5 : 1};
 `
 const CategoryIcon = styled.p`
   font-size: 32px;
