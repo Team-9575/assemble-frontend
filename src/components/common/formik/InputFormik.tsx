c09464fa95241acf9a82c91ee6a853d864adb1da
Hyeon1445 <lily1445@gmail.com>
2023-01-08 18:53:55 +0900

    feat: implement menu drawer

--- src/components/common/formik/InputFormik.tsx
+++ src/components/common/formik/InputFormik.tsx
@@ -6,24 +6,36 @@ interface IInputFormikProps {
   name: string
   label?: string
   isNumber?: boolean
+  hasComma?: boolean
+  max?: number
 }
 
 const InputFormik = ({
   label = '',
   name,
   isNumber = false,
+  hasComma = false,
+  max,
 }: IInputFormikProps) => {
   return (
     <Field name={name}>
       {({ form: { values, setFieldValue }, meta: { value } }: FieldProps) => (
         <Input
           label={label}
-          value={value}
+          value={value ?? ''}
           onChange={(newValue) => {
+            let filteredValue = newValue
+            if (max && max < newValue.length) {
+              filteredValue = newValue.substring(0, max)
+            }
             if (isNumber) {
-              setFieldValue(name, newValue.replace(regex.notNumber, ''))
+              const numberValue = filteredValue.replace(regex.notNumber, '')
+              setFieldValue(
+                name,
+                hasComma ? Number(numberValue).toLocaleString() : numberValue
+              )
             } else {
-              setFieldValue(name, newValue)
+              setFieldValue(name, filteredValue)
             }
           }}
         />
--- src/components/party-detail/MenuDrawer.tsx
+++ src/components/party-detail/MenuDrawer.tsx
@@ -8,10 +8,9 @@ import { Form, Formik } from 'formik'
 import * as Yup from 'yup'
 
 export enum PayType {
-  // TODO: check type
   Individual,
-  Group,
   All,
+  Group,
 }
 
 interface IMenuDrawerProps {
@@ -21,8 +20,8 @@ interface IMenuDrawerProps {
 const initialValues = {
   name: '',
   description: '',
-  price: 0,
-  payType: 0,
+  price: null,
+  payType: null,
 }
 
 const validationSchema = Yup.object({
@@ -31,6 +30,24 @@ const validationSchema = Yup.object({
   price: Yup.string().required('required'),
   payType: Yup.number().required('required'),
 })
+
+const payType = [
+  {
+    name: '개인 메뉴',
+    value: PayType.Individual,
+    description: '(개인별로 정산)',
+  },
+  {
+    name: '전체 메뉴',
+    value: PayType.All,
+    description: '(전체 파티원 수로 정산)',
+  },
+  {
+    name: '그룹 메뉴',
+    value: PayType.Group,
+    description: '(해당 메뉴를 선택한 인원 수로 정산)',
+  },
+]
 
 const MenuDrawer = ({ onClose }: IMenuDrawerProps) => {
   return (
@@ -61,6 +78,23 @@ const MenuDrawer = ({ onClose }: IMenuDrawerProps) => {
                 <Title>메뉴 추가하기</Title>
                 <VStack gap="0.375rem">
                   <OptionTitle>메뉴 유형을 선택해주세요.</OptionTitle>
+                  {payType.map((type) => (
+                    <MenuType
+                      key={type.value}
+                      hasType={values.payType !== null}
+                      isSelected={values.payType === type.value}
+                      type="button"
+                      onClick={() => {
+                        setFieldValue(
+                          'payType',
+                          values.payType === type.value ? null : type.value
+                        )
+                      }}
+                    >
+                      {type.name}{' '}
+                      <TypeDescription>{type.description}</TypeDescription>
+                    </MenuType>
+                  ))}
                 </VStack>
                 <VStack gap="0.375rem">
                   <OptionTitle>메뉴 명을 입력해주세요.</OptionTitle>
@@ -71,10 +105,17 @@ const MenuDrawer = ({ onClose }: IMenuDrawerProps) => {
                   <InputFormik
                     label="메뉴 가격을 입력해주세요."
                     name="price"
+                    hasComma
                     isNumber
+                    max={18}
                   />
                 </VStack>
-                <Button text="완료" isDisabled={!isValid} onClick={() => {}} />
+                <Button
+                  text="완료"
+                  isDisabled={!isValid}
+                  onClick={() => {}}
+                  type="submit"
+                />
               </VStack>
             </Form>
           )}
@@ -93,5 +134,20 @@ const Title = styled.p`
 `
 const OptionTitle = styled.p`
   font-size: ${theme.fontSize.sm};
+`
+const MenuType = styled.button<{ hasType: boolean; isSelected: boolean }>`
+  font-weight: bold;
+  padding: 0.75rem;
+  border: ${({ isSelected }) =>
+    isSelected ? '1px solid #000000' : '1px solid #dbdbdb'};
+  color: ${({ hasType, isSelected }) =>
+    !hasType || isSelected ? '#000000' : '#757575'};
+  width: 100%;
+  text-align: left;
+  border-radius: 10px;
+`
+const TypeDescription = styled.span`
+  font-size: ${theme.fontSize.xs};
+  font-weight: normal;
 `
 export default MenuDrawer
