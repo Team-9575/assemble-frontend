6996f1f9450396a64e7ea807139f953ae884c23a
Hyeon1445 <76871403+Hyeon1445@users.noreply.github.com>
2023-01-09 00:54:44 +0900

    feat: implement individual receipt (#58)
    * feat: implement individual receipt
    
    * fix: fix overflow style

--- src/pages/review/index.tsx
+++ src/pages/review/index.tsx
@@ -1,13 +1,28 @@
 import BaseLayout from '@components/common/base-layout'
 import Button from '@components/common/button'
 import VStack from '@components/common/stack/VStack'
+import IndividualReceipt from '@components/party-detail/IndividualReceipt'
+import { useState } from 'react'
 
 const MyReviewPage = () => {
+  const [isIndividualReceiptOpen, setIsIndividualReceiptOpen] = useState(false)
   return (
     <BaseLayout>
       <VStack gap="1rem" padding="1rem">
         <p>Modals</p>
-        <Button variant="outlined" text="파티원 영수증" onClick={() => {}} />
+        <Button
+          variant="outlined"
+          text="파티원 영수증"
+          onClick={() => {
+            setIsIndividualReceiptOpen(true)
+          }}
+        />
+        <IndividualReceipt
+          isOpen={isIndividualReceiptOpen}
+          onClose={() => {
+            setIsIndividualReceiptOpen(false)
+          }}
+        />
         <Button
           variant="outlined"
           text="파티장 영수증(전체)"
