b4f9a494f26357a19c439fc3c7ec08cc5c4862a7
Hyeon1445 <lily1445@gmail.com>
2023-01-09 00:09:18 +0900

    feat: add dummy modal buttons

--- src/pages/review/index.tsx
+++ src/pages/review/index.tsx
@@ -1,9 +1,20 @@
 import BaseLayout from '@components/common/base-layout'
+import Button from '@components/common/button'
+import VStack from '@components/common/stack/VStack'
 
 const MyReviewPage = () => {
   return (
     <BaseLayout>
-      <div>review</div>
+      <VStack gap="1rem" padding="1rem">
+        <p>Modals</p>
+        <Button variant="outlined" text="파티원 영수증" onClick={() => {}} />
+        <Button
+          variant="outlined"
+          text="파티장 영수증(전체)"
+          onClick={() => {}}
+        />
+        <Button variant="outlined" text="리뷰" onClick={() => {}} />
+      </VStack>
     </BaseLayout>
   )
 }
